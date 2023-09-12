a5d17f25b46ab66f4ab5753f1cf6bb2fcc4e5939
Jose Guillen <Jose.Guillen@thomsonreuters.com>
2023-09-12 11:47:48 -0600

    Changed response and added average value in all

--- app/backend/app.py
+++ app/backend/app.py
@@ -10,15 +10,21 @@ app = Flask(__name__)
 @app.route("/get_main_data", methods=["GET"])
 def initial_dashboard():
     queries = Queries()
-    if not request.args.get('column_name'):
-        return queries.get_main_data()
-    else:
-        return queries.average_reviews_per_year(request.args.get('column_name'), 
-                                                request.args.get('country'), 
-                                                request.args.get('city'), 
-                                                request.args.get('job_title'), 
-                                                request.args.get('cluster'),
-                                                request.args.get('operation_type'))
+    return queries.get_main_data(request.args.get('country'),
+                                 request.args.get('city'),
+                                 request.args.get('job_title'),
+                                 request.args.get('cluster'),
+                                 request.args.get('operation_type'))
+        
+@app.route("/get_column_data", methods=["GET"])
+def column_information():
+    queries = Queries()
+    return queries.average_reviews_per_year(request.args.get('column_name'),
+                                            request.args.get('country'),
+                                            request.args.get('city'), 
+                                            request.args.get('job_title'), 
+                                            request.args.get('cluster'),
+                                            request.args.get('operation_type'))
 
 
 if __name__ == "__main__":
--- app/backend/queries/queries.py
+++ app/backend/queries/queries.py
@@ -11,54 +11,75 @@ class Queries():
         self.df['review_date'] = pd.to_datetime(self.df['Review Date'], format='%d/%m/%y')
         self.df["year"] = self.df['review_date'].dt.year
     
-    def get_main_data(self):
-        result = {
-            'totalReviews':self.total_reviews(),
-            'avgSenior': self.average_reviews_per_year('Senior Leadership', operation_type = 'mean'),
-            'avgCarrer': self.average_reviews_per_year('Career Opportunities', operation_type = 'mean'),
-            'avgCulture': self.average_reviews_per_year('Culture & Values', operation_type = 'mean'),
-            'avgCompensation': self.average_reviews_per_year('Compensation & Benefits', operation_type = 'mean'),
-            'avgDiversity': self.average_reviews_per_year('Diversity & Inclusion', operation_type = 'mean'),
-            'avgWork': self.average_reviews_per_year('Work-Life Balance', operation_type = 'mean')
-            }
+    def get_main_data(self, country = '', city = '', job_title = '', cluster = '', operation_type = ''):
+        result = [
+            self.total_reviews(country, city, job_title, cluster),
+            self.average_reviews_per_year('Senior Leadership', country, city, job_title, cluster, operation_type),
+            self.average_reviews_per_year('Career Opportunities', country, city, job_title, cluster, operation_type),
+            self.average_reviews_per_year('Culture & Values', country, city, job_title, cluster, operation_type),
+            self.average_reviews_per_year('Compensation & Benefits', country, city, job_title, cluster, operation_type),
+            self.average_reviews_per_year('Diversity & Inclusion', country, city, job_title, cluster, operation_type),
+            self.average_reviews_per_year('Work-Life Balance', country, city, job_title, cluster, operation_type)
+        ]
         return json.dumps(result)
     
-    def total_reviews(self):
+    def total_reviews(self, country = '', city = '', job_title = '', cluster = ''):
         x = []
         y = []
         
-        reviews_count = list(self.df.groupby('year').size())[::-1]
-        years = self.df['year'].unique()
+        df_filtered = self.df
+        if country:
+            df_filtered = df_filtered[df_filtered['Review Country Name'] == country]
+        if city:
+            df_filtered = df_filtered[df_filtered['Review City Name'] == city]
+        if job_title:
+            df_filtered = df_filtered[df_filtered['Job Title'] == job_title]
+        if cluster:
+            df_filtered = df_filtered[df_filtered['cluster'] == cluster]
+            
+            
+        reviews_count = list(df_filtered.groupby('year').size())[::-1]
+        years = df_filtered['year'].unique()
         
         for r in zip(years, reviews_count):
             x.insert(0,str(r[0]))
             y.insert(0,int(r[1]))
-            
-        return {'title':'Total reviews per year', 'x':x, 'y':y}
+        
+        average = sum(y)
+        return {'title':'Total reviews per year', 'x':x, 'y':y, 'average':average}
     
-    def average_reviews_per_year(self, column_name, country = '', city = '', job_title = '', cluster = '', operation_type = 'mean'):
+    def average_reviews_per_year(self, column_name, country = '', city = '', job_title = '', cluster = '', operation_type = ''):
         x = []
         y = []
-            
-        years = self.df['year'].unique()
+        operation_text = ''
+        
+        df_filtered = self.df
+        if country:
+            df_filtered = df_filtered[df_filtered['Review Country Name'] == country]
+        if city:
+            df_filtered = df_filtered[df_filtered['Review City Name'] == city]
+        if job_title:
+            df_filtered = df_filtered[df_filtered['Job Title'] == job_title]
+        if cluster:
+            df_filtered = df_filtered[df_filtered['cluster'] == cluster]
+        
+        print(df_filtered)
+        years = df_filtered['year'].unique()
         for year in years:
-            x.insert(0, int(year))
-            df_filtered = self.df[self.df['year']== year]
-            if country:
-                df_filtered = df_filtered[df_filtered['Review Country Name'] == country]
-            if city:
-                df_filtered = df_filtered[df_filtered['Review City Name'] == city]
-            if job_title:
-                df_filtered = df_filtered[df_filtered['Job Title'] == job_title]
-            if cluster:
-                df_filtered = df_filtered[df_filtered['cluster'] == cluster]
+            x.insert(0, str(year))
+            df_year_filtered = df_filtered[df_filtered['year'] == year]
                 
-                
+            print(df_year_filtered[column_name])
             if(operation_type == 'mean' or operation_type == None):
-                y.insert(0, round(df_filtered[column_name].mean(), 3))
+                y.insert(0, float(df_year_filtered[column_name].mean()))
+                operation_text = 'Average '
             elif(operation_type == 'max'):
-                y.insert(0, round(df_filtered[column_name].max(), 3))
+                y.insert(0, float(df_year_filtered[column_name].max()))
+                operation_text = 'Maximum '
             elif(operation_type == 'min'):
-                y.insert(0, round(df_filtered[column_name].min(), 3))
+                y.insert(0, float(df_year_filtered[column_name].min()))
+                operation_text = 'Minimum '
+        
+        average = sum(y)/len(y)
             
-        return {'title':'Average ' + column_name + ' per year', 'x':x, 'y':y}
+        return {'title': operation_text + column_name + ' per year', 'x':x, 'y':y, 'average':average}
